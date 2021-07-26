3da24ad5bca15a80f1bbf686a7185c8e4e80d2b3
No1stone <jangws1003@naver.com>
2021-07-26 14:52:19 +0900

    git first

---
+++ inflean-restapi-st/src/main/java/com/infrest/st/InfleanRestapiStApplication.java
@@ -0,0 +1,13 @@
+package com.infrest.st;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfleanRestapiStApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfleanRestapiStApplication.class, args);
+	}
+
+}
---
+++ inflean-restapi-st/src/test/java/com/infrest/st/InfleanRestapiStApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infrest.st;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfleanRestapiStApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
